6b58aae26c219de69d09dd67f7bd11ef751c9521
Beniszabo <150236130+Beniszabo@users.noreply.github.com>
2024-01-18 15:58:56 +0100

    Add files via upload

---
+++ jelszo.py
@@ -0,0 +1,11 @@
+felhasznalonev=input('Adja meg a felhasználónevét!')
+jelszo=input('Adja meg a jelszavát!')
+
+f='bori99'
+j='Szivecske<3'
+
+
+if f==felhasznalonev and j==jelszo:
+    print('Belépés engedélyezve')
+else:
+    print('Belépés megtagadva')
