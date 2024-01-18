f437df8ab54c340fac627595f9de7a0c1bb94ca8
Beniszabo <150236130+Beniszabo@users.noreply.github.com>
2024-01-18 16:12:51 +0100

    Add files via upload

---
+++ 2.feladat.py
@@ -0,0 +1,8 @@
+print('2. feladat: Szökőév listázó')
+evszam1=int(input('Kérem az egyik évszámot: '))
+evszam2=int(input('Kérem az másik évszámot: '))
+
+szokoevek=[]
+
+def szokoev_meghatarozasa()
+    
